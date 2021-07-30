ee5b1fbe0fc159e58690ffaf5c9d2d664d5b5422
noaht283 <88215641+noaht283@users.noreply.github.com>
2021-07-30 19:51:34 -0400

    Update GreetingCardPrint.java

--- GreetingCardPrint.java
+++ GreetingCardPrint.java
@@ -1,59 +1,32 @@
-mport java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+public class GreetingCardPrinter {
 
-/**
- * Homework 6 Card Creator
- * 
- * This class defines the thread task that will "come up with" and submit
- * greeting card ideas to the print queue. We have added the code necessary to
- * read from the file, but it's up to you to handle turning off the printer
- * (keeping track of how many threads are open) and adding the read-in line from
- * the inspiration file to the queue.
- * 
- * @author
- *
- */
-public class CardCreator implements Runnable {
-
-	/**
-	 * Print queue to add new card ideas
-	 */
-	private PrintQueue printQueue;
-
-	public int threadCount = 0;
-
-	/**
-	 * Inspiration file name
-	 */
-	private String filename;
-
-	public CardCreator(PrintQueue d, String filename) {
-		printQueue = d;
-		this.filename = filename;
+	public static void main(String[] args) {
+		// Create a shared print queue
+		PrintQueue d = new PrintQueue();
+		
+		// Create two card creators that will imagine their
+		// ideas by reading the prompts from birthday.txt
+		// and roses.txt.  (I.e. a little hard-coded
+		// inspiration!)
+		CardCreator r1 = new CardCreator(d, "birthday.txt");
+		CardCreator r2 = new CardCreator(d, "roses.txt");
+		
+		// Printer will try to print the cards by checking the
+		// print queue.
+		Printer r3 = new Printer(d, 1000);
+		Printer r4 = new Printer(d, 1000);
+		
+		
+		// Create new threads to run each of the Runnables
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		Thread t3 = new Thread(r3);
+		Thread t4 = new Thread(r4);
+		
+		// Start the threads
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 	}
-
-	/**
-	 * Run method that is the main method for the thread
-	 */
-	@Override
-	public void run() {
-		Scanner s = null;
-		threadCount++;
-		try {
-			s = new Scanner(new FileReader(filename));
-			while (s.hasNextLine()) {
-				this.printQueue.enqueue(s.nextLine());
-			}
-		} catch (IOException e) {
-			System.out.println("Could not read file");
-		} finally {
-			if (s.hasNextLine() == false && threadCount == 0) {
-				s.close();
-				printQueue.turnOff();
-			}
-		}
-
-	}
-
 }
